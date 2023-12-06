f770921ea8ac62b4351162dd0aaba98ff854d4f1
oliviajacob <152943305+oliviajacob@users.noreply.github.com>
2023-12-06 17:58:00 -0500

    Update NutritionPlan.py

--- flask-app/src/NutritionPlan/NutritionPlan.py
+++ flask-app/src/NutritionPlan/NutritionPlan.py
@@ -13,7 +13,18 @@ nutrition_plan = Blueprint('NutrionPlan', __name__)
 @nutrition_plan.route('/NutritionPlan', methods=['GET'])
 def get_nutrition_plans():
     # Logic to retrieve and return all nutrition plans
-    pass
+    cursor = db.get_db().cursor()
+    cursor.execute('select Goals, DietRestrictions, MealPlan, DietType,\
+                    MemberID from NutritionPlan')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
 
 @nutrition_plan.route('/NutritionPlan', methods=['POST'])
 def add_nutrition_plan():
