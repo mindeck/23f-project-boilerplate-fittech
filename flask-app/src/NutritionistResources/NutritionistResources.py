4779bc9c2a879ac1defd6456275e65e56e937b74
oliviajacob <152943305+oliviajacob@users.noreply.github.com>
2023-12-06 17:14:18 -0500

    Update and rename NutritionistResoruces.py to NutritionistResources.py

--- flask-app/src/NutritionistResources/NutritionistResources.py
+++ flask-app/src/NutritionistResources/NutritionistResources.py
@@ -12,8 +12,19 @@ nutritionist_resources = Blueprint('NutritionistResources', __name__)
 
 @nutritionist_resources.route('/NutritionistResources', methods=['GET'])
 def get_nutritionist_resources():
-    # Logic to retrieve and return all trainer resources
-    pass
+    # Logic to retrieve and return all nutritionist resources
+    cursor = db.get_db().cursor()
+    cursor.execute('select Food, Calories,\
+        Nutrients, Protein, Carbs, NutritionistID from NutritionistResources')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
 
 @nutritionist_resources.route('/NutritionistResources', methods=['POST'])
 def add_nutritionist_resources():
