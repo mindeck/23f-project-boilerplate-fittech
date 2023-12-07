ee9d2219b5ac95d992b8d3fba2630640c9b0c7e6
mindeck <indeckmarkus@gmail.com>
2023-12-07 14:06:10 -0500

    update

--- flask-app/src/Classes/Classes.py
+++ flask-app/src/Classes/Classes.py
@@ -15,7 +15,7 @@ def get_classes():
     # Logic to retrieve and return all classes
     cursor = db.get_db().cursor()
     cursor.execute('select ClassID, SpotsAvailable,\
-        StartTime, MembershipID from Classes')
+        StartTime, MemberID from Classes')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -31,14 +31,30 @@ def add_class():
     class_name = request.form['ClassName']
     spots_available = request.form['SpotsAvailable']
     start_time = request.form['StartTime']
-    membership_id = request.form['MembershipID']
-    # Insert logic
-    # ...
+    membership_id = request.form['MemberID']
+    cursor = db.get_db().cursor()
+        error = None
+
+        if error is None:
+            try:
+                cursor.execute(
+                    "INSERT INTO Classes (ClassName, SpotsAvailable, StartTime, MemberID) VALUES (?, ?, ?, ?,)",
+                    (class_name, spots_available, start_time, membership_id),
+                )
+                cursor.commit()
+            except cursor.IntegrityError:
+                error = f"Classes {class_name, start_time} already exists."
+        else:
+            return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template('auth/register.html')
 
 @classes.route('/Classes/<int:class_id>', methods=['PUT'])
 def update_class(class_id):
     class_name = request.form['ClassName']
     spots_available = request.form['SpotsAvailable']
     start_time = request.form['StartTime']
-    membership_id = request.form['MembershipID']
+    membership_id = request.form['MemberID']
     #
